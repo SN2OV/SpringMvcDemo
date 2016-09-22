4828487eaf77da08e2b1a8e535f1d413676ced42
SN2OV <843390732@qq.com>
2016-09-23 00:32:33 +0800

    客流调查系统搭建好环境初版

---
+++ code/SubSurveyServer/src/main/Java/cn/sn2ov/subsurvey/Controller/MainController.java
@@ -0,0 +1,22 @@
+package cn.sn2ov.subsurvey.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by SN2OV on 2016/9/20.
+ */
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/a", method = RequestMethod.GET)
+    public String index() {
+        return "indexa";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String test(){
+        return "indexaa";
+    }
+}
